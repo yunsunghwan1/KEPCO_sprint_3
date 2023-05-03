3dbc95256a32498419d2501b336101d01c555b51
yunsunghwan1 <seonghwanyun8@gmail.com>
2023-05-03 19:09:31 +0900

    거의 수정 끝

--- sprint_3/mainapp/map/map_view_power.py
+++ sprint_3/mainapp/map/map_view_power.py
@@ -6,21 +6,21 @@ import folium
 import json
 
 
-class Map_View :
+class Map_View_power :
 
     ### 클래스 생성시 아래 함수 모두 실행시키기
     # - 생성자에서 호출
-    def __init__(self, input_data) :
+    def __init__(self) :
         self.initDataFrame()
         self.map_base()
-        self.map_location(input_data)
+        self.map_location()
 
 
     ### 데이터 읽어들이기
     def initDataFrame(self) :
-        file_path = "./mainapp/map/locpeople_df.csv"
+        file_path = "./mainapp/map/20201230all2.csv"
         # self.seoul_starbucks = pd.read_csv(file_path, encoding=)
-        self.local_population = pd.read_csv(file_path, encoding='cp949')
+        self.local_population = pd.read_csv(file_path)
         self.jsonfile = open('./mainapp/map/TL_SCCO_CTPRVN.json', 'r', 
                 encoding='utf8').read()
         self.jsondata = json.loads(self.jsonfile)
@@ -44,14 +44,14 @@ class Map_View :
 
 
     ### 스타벅스 타입별 매장위치 표시하기
-    def map_location(self, input_data) :
+    def map_location(self) :
         folium.Choropleth(
             geo_data = self.jsondata, # 경계선 좌표값이 담긴 데이터
             data = self.local_population, # Series or DataFrame 넣으면 된다
-            columns = ['행정구역별', input_data ], # DataFrame의 어떤 columns을 넣을지
+            columns = ['세부지역', '설비용량'], # DataFrame의 어떤 columns을 넣을지
             key_on = 'feature.properties.CTP_KOR_NM', # id 값을 가져오겠다; feature.id : feature 붙여줘야 함 (folium의 정해진 형식)
-            fill_color = 'YlOrRd',
-            fill_opacity = 0.5, # 색 투명도
+            fill_color = 'BuPu',
+            fill_opacity = 0.7, # 색 투명도
             line_opacity = 0.5, # 선 투명도
             legend_name = '년 인구 수', # 범례
             highlight=True,
